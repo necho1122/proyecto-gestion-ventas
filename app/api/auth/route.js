ae13dfd5515d604000eff8014e0476e2bb8c5ed9
necho1122 <nelsonubac90@gmail.com>
2025-01-19 17:29:07 -0300

    son

--- app/api/auth/route.js
+++ app/api/auth/route.js
@@ -1,25 +1,10 @@
+import { auth } from '@/lib/firebase';
 import {
-	getAuth,
 	signInWithEmailAndPassword,
 	sendPasswordResetEmail,
 } from 'firebase/auth';
-import { initializeApp } from 'firebase/app';
 import { NextResponse } from 'next/server';
 
-// Configuración de Firebase
-const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Manejo de métodos HTTP
 export async function POST(request) {
 	const body = await request.json();
 	const { email, password, action } = body;
